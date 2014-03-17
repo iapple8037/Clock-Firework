89f9f5fdf66fd1e263ee53a81aa21272e0d030c7
HIROKI <YANAGIDA@yanagida-no-MacBook-Air.local>
2014-03-18 00:11:45 +0900

    Initial Commit

---
+++ Clock Firework/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Clock Firework
+//
+//  Created by HIROKI on 2014/03/18.
+//  Copyright (c) 2014年 HIROKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Clock Firework/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Clock Firework
+//
+//  Created by HIROKI on 2014/03/18.
+//  Copyright (c) 2014年 HIROKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
